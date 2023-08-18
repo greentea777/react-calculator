d7031c0820fe7b7b455ee7daa866ce311ff33687
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-08-18 01:34:36 -0700

    added comments

--- src/components/App.js
+++ src/components/App.js
@@ -7,27 +7,34 @@ import { useState } from "react";
 import Display from "./Display";
 
 function App() {
+  // display text
   const [outputText, setOutputText] = useState("0");
+  // calculation state
   const [outputValue, setOutputValue] = useState("0");
-
-  const [operatorClicked, setOperatorClicked] = useState(false);
+  // operator btn state
+  const [isOperatorClicked, setIsOperatorClicked] = useState(false);
+  // equal btn state
   const [isEqual, setIsEqual] = useState(false);
 
   const handleDisplay = (value, type, text) => {
+    // if init num 0, replace 0 to input num
     if (outputText.length === 1 && outputText === "0" && type === "number") {
       setOutputText(text.toString());
       setOutputValue(value.toString());
     } else {
+      // check btn type
       switch (type) {
         case "number":
+          // if user has clicked equal btn
           if (isEqual) {
+            // rest calculator
             setOutputText(text.toString());
             setOutputValue(value.toString());
-            setOperatorClicked(false);
+            setIsOperatorClicked(false);
             setIsEqual(false);
-
             return;
           } else {
+            // avoid user clicking multi zero
             const lastChar = outputText
               .toString()
               .charAt(outputText.length - 1);
@@ -35,11 +42,13 @@ function App() {
             const lastTwoChar = outputText
               .toString()
               .charAt(outputText.length - 2);
-
+            // check if second-to-last is not a num, and last char is zero
             if (isNaN(parseInt(lastTwoChar)) && lastChar === "0") {
+              // if input 0, end function
               if (text.toString() === "0") {
                 return;
               } else {
+                // replace last char to new input
                 let newText = outputText
                   .toString()
                   .substring(0, outputText.length - 1);
@@ -55,12 +64,13 @@ function App() {
             } else {
               setOutputValue((o) => (o += value));
               setOutputText((o) => (o += text));
-              setOperatorClicked(false);
+              setIsOperatorClicked(false);
             }
           }
           break;
         case "operator":
-          if (operatorClicked) {
+          // allow user to replace operator
+          if (isOperatorClicked) {
             let newText = outputText
               .toString()
               .substring(0, outputText.length - 1);
@@ -75,26 +85,28 @@ function App() {
           } else {
             setOutputValue((o) => (o += value));
             setOutputText((o) => (o += text));
-            setOperatorClicked(true);
+            setIsOperatorClicked(true);
             setIsEqual(false);
           }
 
           break;
         case "enter":
           const lastChar = outputText.toString().charAt(outputText.length - 1);
-
+          // if last char is not a num, end function
           if (isNaN(parseInt(lastChar))) {
             return;
           } else {
+            // calculate result and rest values
             setOutputText(eval(outputValue).toString());
             setOutputValue(eval(outputValue).toString());
-            setOperatorClicked(false);
+            setIsOperatorClicked(false);
             setIsEqual(true);
           }
-
           break;
         case "clear":
+          // AC or C
           if (value === "Clear") {
+            // if display e and error message, clear all
             if (
               outputText.toString().includes("e") ||
               outputText.toString() === "Infinity" ||
@@ -102,16 +114,17 @@ function App() {
             ) {
               setOutputText("0");
               setOutputValue("0");
-              setOperatorClicked(false);
+              setIsOperatorClicked(false);
               setIsEqual(false);
             } else {
+              // delete one by one from last char
               let newText = outputText
                 .toString()
                 .substring(0, outputText.length - 1);
               let newValue = outputValue
                 .toString()
                 .substring(0, outputValue.length - 1);
-
+              // if display value is empty, set it back to 0
               if (newText === "") {
                 setOutputText("0");
                 setOutputValue("0");
@@ -121,12 +134,12 @@ function App() {
               }
             }
           } else {
+            // AC, reset
             setOutputText("0");
             setOutputValue("0");
-            setOperatorClicked(false);
+            setIsOperatorClicked(false);
             setIsEqual(false);
           }
-
           break;
       }
     }
